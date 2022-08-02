4d94b339ce3c90ba4ea9d5ae8b978f928058de16
KonniLol <konnilol@outlook.com>
2022-08-02 12:09:37 +0200

    nvim

---
+++ .config/nvim/lua/konnilol/init.lua
@@ -0,0 +1,6 @@
+require("konnilol.plugins")
+require("konnilol.options")
+require("konnilol.keymaps")
+require("konnilol.colorscheme")
+require("konnilol.cmp")
+require("konnilol.telescope")
---
+++ .config/nvim/lua/konnilol/plugins.lua
@@ -0,0 +1,38 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  use 'nvim-lua/popup.nvim'
+  use 'normen/vim-pio'
+  -- use 'github/copilot.vim'
+  use 'bfrg/vim-cpp-modern'
+  use "nvim-lua/plenary.nvim"
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
